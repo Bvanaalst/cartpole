fabff9f635e20af2f83b884601832b3841147013
MaartenWI <maartenwilders@gmail.com>
2024-03-18 17:53:46 +0100

    update DQN_attempt2

--- DQN_Attempt1.py
+++ DQN_Attempt1.py
@@ -1,94 +1,97 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import random
 from collections import deque
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
-import gym
+import gymnasium as gym
+from matplotlib import pyplot as plt
 
 
-# In[2]:
+class DQAgent:
+    def __init__(self, state_size, action_size, replay_memory_capacity=1000, epsilon=0.5):
+        self.state_size = state_size  # Set output size for Q-function
+        self.action_size = action_size  # Set input size for Q-function
+        self.memory = deque(maxlen=replay_memory_capacity)  # Initialize replay memory to capacity N
+        self.gamma = 0.95  # Future rewards are discounted by gamma per time-step
+        self.learning_rate = 0.001  # Learning rate for gradient descent
+        self.action_value_function = self._build_model()  # Initialize action-value function Q with random weights
+        self.epsilon = epsilon  # Probability for random action
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.01
 
-
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=2000)  
-        self.gamma = 0.95 
-        self.learning_rate = 0.001
-        self.model = self._build_model()
-        
-        
-    def _build_model(self):
+    def _build_model(self):  # Takes state as input, outputs Q values for legal actions
         model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(24, activation='relu'))
+        model.add(Dense(64, input_dim=self.state_size, activation='tanh'))
+        model.add(Dense(32, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
-    
-    def buffer(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-        
+
+    def store_transition(self, s, a, r, s_next, done):  # Stores transition in replay memory
+        self.memory.append((s, a, r, s_next, done))
+
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-            if not done:
-                target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
-            target_f = self.model.predict(state)
-            print(target_f[0])
-            target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
-            
-    def act(self, state):
-        return np.argmax(self.model.predict(state)[0])
-    
+        for s, a, r, s_next, done in minibatch:
+            if done:
+                y = r
+            else:
+                Q_next = np.amax(self.action_value_function.predict(s_next, verbose=0)[0])
+                y = r + self.gamma * Q_next
+            Q = self.action_value_function.predict(s, verbose=0)
+            Q[0][a] = y
 
-    
-env = gym.make('CartPole-v1')
-state_size = env.observation_space.shape[0]
-action_size = env.action_space.n
+            self.action_value_function.fit(s, Q, verbose=0) # Keras builds the loss function akin to equation 3 as outlined in the DQN paper.
 
-agent = DQNAgent(state_size, action_size)
+    def select_action(self, s):
+        if self.epsilon >= np.random.rand():
+            return env.action_space.sample()  # Return a random action
+        else:
+            a = self.action_value_function.predict(s, verbose=0)  # Get Q-values from Q-function
+            return np.argmax(a[0])  # Return the action associated with the maximum Q-value
 
-episodes = 10
-batch_size = 32
-
-for e in range(episodes):
-    state, _ = env.reset()
-    print("State shape:", state)
-    state = np.array([state]).reshape([1, state_size])
-    done = False
-    total_reward = 0
-
-    while not done:
-        
-        action = agent.act(state)
-        step_result = env.step(action)
-        next_state, reward, done, _ = step_result[:4]
-        next_state = np.reshape(next_state, [1, state_size])
-        agent.buffer(state, action, reward, next_state, done)
-        state = next_state
-        total_reward += reward
-        
-        if len(agent.memory) > batch_size:
-            agent.replay(batch_size)
-    
-    print("Episode: {}, Total Reward: {}".format(e+1, total_reward))
-
-env.close()
+    def annealing_epsilon(self, ):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
 
-# In[ ]:
+if __name__ == "__main__":
 
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQAgent(state_size, action_size)
+    # print(state_size)
+    episodes = 50
+    batch_size = 16
+    rewards = []
 
+    for e in range(episodes):
+        s, _ = env.reset()
+        # print('s1: ', s)
+        # print("State shape:", s)
+        s = np.array([s]).reshape([1, state_size])
+        # print('s2: ', s)
+        done = False
+        time = 0
+        while not done:
+            a = agent.select_action(s)
+            step_result = env.step(a)  # Execute action_t in emulator and observe next_state, reward and terminal state
+            s_next, r, done, _ = step_result[:4]
+            s_next = np.reshape(s_next, [1, state_size])
+            agent.store_transition(s, a, r, s_next, done)
+            state = s_next
 
+            if len(agent.memory) > batch_size: # Only perform experience replay once memory is full
+                agent.replay(batch_size)
 
+            time += 1
+            agent.annealing_epsilon()
+
+        print('episode:', e + 1, 'time:', time)
+
+        # print("Episode: {}, Total Reward: {}".format(e+1, total_reward))
+    print(rewards)
+    plt.plot(rewards)
+    env.close()
