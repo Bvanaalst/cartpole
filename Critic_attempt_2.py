028311b3309a858182ccae015947f006c6294816
MaartenWI <maartenwilders@gmail.com>
2024-04-16 12:42:51 +0200

    Update Critic_attempt_2.py

--- Critic_attempt_2.py
+++ Critic_attempt_2.py
@@ -1,16 +1,14 @@
 import numpy as np
-import random
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, Input, Concatenate
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
-import keras.backend as K
 import gymnasium as gym
 
 
 class Agent(object):
-    def __init__(self, ALPHA=1e-4, GAMMA=0.99, n_actions=2, state_size=4):
-        self.gamma = GAMMA
-        self.lr = ALPHA
+    def __init__(self, alpha=1e-4, gamma=0.99, n_actions=2, state_size=4):
+        self.gamma = gamma
+        self.lr = alpha
         self.G = 0
         self.state_size = state_size
         self.n_actions = n_actions
@@ -41,17 +39,12 @@ class Agent(object):
         encoded_action = np.zeros(self.n_actions)                                            # one-hot encoding
         encoded_action[action] = 1
 
-        self.gradient_memory.append(np.array(encoded_action).astype('float32') - probabilities)
-        self.probabilities.append(probabilities)
         self.state_memory.append(observation)
         self.action_memory.append(encoded_action)
         self.reward_memory.append(reward)
 
     def update_policy(self):
-        #state_memory = np.array(self.state_memory)
-        #action_memory = np.array(self.action_memory)
         reward_memory = np.array(self.reward_memory)
-
 
         G = np.zeros_like(reward_memory)
         for t in range(len(reward_memory)):
@@ -65,33 +58,24 @@ class Agent(object):
         std = np.std(G) + 1e-9
         self.G = (G - mean) / std
 
-        gradients = np.vstack(self.gradient_memory)
-        discounted_rewards = np.vstack(self.G)
-        gradients *= discounted_rewards
         states = np.squeeze(np.vstack([self.state_memory]))
         actions = np.squeeze(np.vstack(self.action_memory))
-        Y = self.probabilities + self.lr * np.vstack([gradients])
-
-        #self.model.train_on_batch(states, Y)
+        print("states: ", states)
+        print('actions: ', actions)
+        print('G: ', self.G)
         self.model.train_on_batch(states, actions, sample_weight=self.G)
 
-        #print('states: ', states)
-        #print('actions: ', actions)
-        #print('G: ', self.G)
-        #print('rewards', reward_memory)
         self.state_memory = []
         self.action_memory = []
         self.reward_memory = []
-        self.gradient_memory = []
-        self.probabilities = []
 
 
 def test():
     env = gym.make('CartPole-v1')
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
-    agent = Agent()
-    n_episodes = 2000
+    agent = Agent(n_actions=action_size, state_size=state_size)
+    n_episodes = 500
     score_history = []
     for i in range(n_episodes):
         done = False
@@ -102,16 +86,12 @@ def test():
             a, prob = agent.choose_action(s)
             step_result = env.step(a)  # Execute action_t in emulator and observe next_state, reward and terminal state
             s_next, r, done, _ = step_result[:4]
-            #print('s_next', s_next)
-
             agent.store_transition(s, a, r, prob)
-
+            s = s_next
             score += r
-
         score_history.append(score)
         agent.update_policy()
         print('episode ', i, ' score ', score, 'average_score ', np.mean(score_history[-100:]))
-
 
 test()
 
